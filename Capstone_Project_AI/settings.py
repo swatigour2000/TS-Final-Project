7b2134f8adcb40604b36b08d6b10eff55793b46c
swatigour2000 <72186365+swatigour2000@users.noreply.github.com>
2022-04-23 18:11:42 +0530

    Update settings.py

--- Capstone_Project_AI/settings.py
+++ Capstone_Project_AI/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wscu3gd9^9&zu=w4_6g75^rcg5&)qkrhg4as$tox)u-x3tzm&_'
+SECRET_KEY = '***************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
